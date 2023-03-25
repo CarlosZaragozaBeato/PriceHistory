e6643e21cfc8bc8a400d2b13c77dec5d158b25c3
Gbatz <carloszaragozabeato15@gmail.com>
2023-03-25 12:21:37 +0100

    s

--- srv_clc/Cliente.py
+++ srv_clc/Cliente.py
@@ -1,22 +1,12 @@
-import socket
+import requests
 
 
-def Cliente(ipServidor:str, puertoServidor:int):
-    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    cliente.connect((ipServidor, puertoServidor))
-    
-    print("Conectado con el servidor ----> %s:%s" %(ipServidor, puertoServidor))
-    
-    while True:
-        msg = input("> ")
-        cliente.send(msg.encode())
-        respuesta = cliente.recv(4096)
-        print(respuesta.decode())
-        if respuesta == "exit":
-            break
-        
-    print("-----------CONEXION CERRADA--------------")
-    cliente.close()
-    
-    
-Cliente('127.0.0.1',8080)
+class Cliente:
+    def __init__(self, url):
+        self.url = url
+
+
+    def EnviarRespuesta(self, mensaje:str):
+        requests.post(self.url, mensaje)
+
+
